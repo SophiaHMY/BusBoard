f1b342087ce80edf39d7bceca2db8af0ffeef70e
Sophia Ho Mei Yin <Sophia.HoMeiYin@softwire.com>
2023-09-25 09:18:47 +0100

    Part 1

---
+++ BusBoard.ConsoleApp/Bus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BusBoard.ConsoleApp
+{
+    public class Bus
+    {
+        public string DestinationName { get; set; }
+        public string ExpectedArrival { get; set; }
+
+        public DateTime convertDate()
+        {
+            return Convert.ToDateTime(ExpectedArrival);
+        }
+    }
+}
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace BusBoard.ConsoleApp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://api.tfl.gov.uk/StopPoint/");
+            var request = new RestRequest("{id}/Arrivals");
+            
+            Console.WriteLine("Enter bus stop code");
+            var stop = Console.ReadLine();
+            request.AddParameter("id", stop);
+            
+            var response = client.Get(request);
+            var content = JsonConvert.DeserializeObject<List<Bus>>(response.Content);
+            var buses = GetBuses(content, 5);
+            Console.WriteLine("Next 5 buses at bus stop " + stop +" :");
+            foreach (var bus in buses)
+            {
+                Console.WriteLine((bus.convertDate()).ToString("h:mm tt") + " " + bus.DestinationName);
+            }
+
+        }
+
+        private static IEnumerable<Bus> GetBuses(IEnumerable<Bus> buses, int amount)
+        {
+            var sortedBuses = buses.OrderBy(bus => bus.ExpectedArrival);
+            return (sortedBuses.ToList()).GetRange(0, amount);
+
+        }
+
     }
-  }
 }
